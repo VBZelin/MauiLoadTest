9e6115694a17d72655638e087efbfaf87b1dc2e8
Stephen Quan <squan@esri.com>
2024-11-13 16:48:32 +1100

    Document WorsePage and BestPage

--- MauiLoadTest/BestPage.xaml.cs
+++ MauiLoadTest/BestPage.xaml.cs
@@ -3,15 +3,34 @@ using System.Diagnostics;
 
 namespace MauiLoadTest;
 
+/// <summary>
+/// This demonstrates some practices to help improve page navigation responsiveness.
+/// </summary>
 public partial class BestPage : ContentPage
 {
+    /// <summary>
+    /// This is a collection that will be populated in a different thread.
+    /// </summary>
     Queue<SampleItem> SampleItemsQueue = new();
+
+    /// <summary>
+    /// This is a collection that is linked to a CollectionView and will be intermittent populated in the UI thread.
+    /// </summary>
     public ObservableCollection<SampleItem> SampleItems { get; } = new();
 
+    /// <summary>
+    /// This is a reference counter that lets us know if we in the process of populating the collection.
+    /// </summary>
     private int Loading = 0;
 
+    /// <summary>
+    /// This cancellation token will be used to abort any loading.
+    /// </summary>
     private CancellationTokenSource CTS = new();
 
+    /// <summary>
+    /// This constructor demonstrates the practice of minimizing constructor implementation.
+    /// </summary>
     public BestPage()
     {
         InitializeComponent();
@@ -21,6 +40,12 @@ public partial class BestPage : ContentPage
         Loading--;
     }
 
+    /// <summary>
+    /// This method demonstrates simulates downloading 12345 records from a REST service in 100 record pages.
+    /// This method takes 3-6 seconds to complete, but, since it occurs in a non-UI thread users will not see the UI block.
+    /// There's a 500ms delay start to minimize the impact to page navigation.
+    /// </summary>
+    /// <returns></returns>
     public async Task LoadItems()
     {
         Loading++;
@@ -39,6 +64,12 @@ public partial class BestPage : ContentPage
         Loading--;
     }
 
+    /// <summary>
+    /// This method demonstrates a periodic update of the UI thread which takes 10ms and occurs every 200ms.
+    /// Records are being transferred from the non-UI collection to the UI collection.
+    /// There's a 500ms delay start to minimize the impact to page navigation.
+    /// </summary>
+    /// <returns></returns>
     public async Task SyncItemsLoop()
     {
         await Task.Delay(500);
@@ -52,6 +83,10 @@ public partial class BestPage : ContentPage
         }
     }
 
+    /// <summary>
+    /// This method copies records from the non-UI collection to the UI collection.
+    /// This method is capped to take no more than 10ms to execute.
+    /// </summary>
     public void SyncItems()
     {
         Stopwatch watch = new();
@@ -65,8 +100,16 @@ public partial class BestPage : ContentPage
         }
     }
 
+    /// <summary>
+    /// Signal a cancellation if a page navigation occurs before the collections finished populating.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
     private void OnDisappearing(object sender, EventArgs e)
     {
-        CTS.Cancel();
+        if (Loading > 0 && SampleItemsQueue.Count > 0)
+        {
+            CTS.Cancel();
+        }
     }
 }
--- MauiLoadTest/WorsePage.xaml.cs
+++ MauiLoadTest/WorsePage.xaml.cs
@@ -2,16 +2,28 @@ using System.Collections.ObjectModel;
 
 namespace MauiLoadTest;
 
+/// <summary>
+/// This is page demonstrates the common problem of doing too much during page navigation/construction.
+/// </summary>
 public partial class WorsePage : ContentPage
 {
+    /// <summary>
+    /// This collection is bound to the CollectionView and must be updated in the UI thread.
+    /// </summary>
 	public ObservableCollection<SampleItem> SampleItems { get; } = new();
 
+    /// <summary>
+    /// Demonstrates the common problem of doing too much during page navigation/construction.
+    /// </summary>
 	public WorsePage()
 	{
 		InitializeComponent();
         LoadItems();
     }
 
+    /// <summary>
+    /// Simulate loading 12345 records from a REST service in 100 record pages blocking the UI thread for 3-6 seconds.
+    /// </summary>
     public void LoadItems()
     {
         for (int i = 0; i < 12345; i++)
